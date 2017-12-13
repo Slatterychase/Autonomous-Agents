7bd04af36a811b4af2b99755d581cff0f397d6d2
Chase Slattery <chs6298@rit.edu>
2017-12-13 00:08:05 -0500

    Added drag force

---
+++ Flocking/Assets/Script/AirResistance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirResistance : MonoBehaviour {
+    public float c = .1f;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public Vector3 checkDrag(Vector3 velocity)
+    {
+        float speed = velocity.magnitude;
+        float dragMagnitude = c * speed * speed;
+        Vector3 drag = velocity;
+        drag = drag * -1;
+        drag = drag.normalized;
+        drag = drag * dragMagnitude;
+        return drag;
+
+
+    }
+}
